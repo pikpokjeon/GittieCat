d6952f4e1d66bbcc25441dfae7c3b1ae12f4c3c5
YaejinJeon <pikpokjeon@gmail.com>
2020-12-09 01:06:34 +0900

    make root view as Profile

--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,13 @@ Vue.use(VueRouter);
 export const router = new VueRouter({
   mode: 'history',
   routes: [
+    // {
+    //   path: '/',
+    //   component: Index,
+    // },
     {
       path: '/',
-      component: Index,
+      component: Profile,  //우선 개발해야 하는 페이지를 루트로
     },
     {
       path: '/Profile',
