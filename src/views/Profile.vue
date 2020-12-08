506ec3c1b0242e2b4bcd768aecac2b9c6adb58eb
YaejinJeon <pikpokjeon@gmail.com>
2020-12-09 01:18:56 +0900

    adding components

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,16 +1,23 @@
 <template>
-<main>
+
   <section>
-
-
-
+    <GameSection></GameSection>
+    <ChartSection></ChartSection>
+    <RelationshipSection></RelationshipSection>
   </section>
-</main>
+
 </template>
 <script>
-
+import ChartSection from './../components/profile/ChartSection'
+import GameSection from './../components/profile/GameSection'
+import RelationshipSection from './../components/profile/RelationshipSection'
 export default {
-  
+  components:{
+    ChartSection,
+    GameSection,
+    RelationshipSection
+
+  }
 }
 </script>
 <style lang="scss" scoped>
