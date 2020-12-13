2a466a770e5b9d7d7c5a6be52105015685229bbb
ildang100 <last2599@gmail.com>
2020-12-13 18:03:40 +0900

    Login Test and push Test

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Profile from '@/views/Profile.vue';
+import Login from '@/components/loginTest/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,10 @@ export const router = new VueRouter({
 		{
 			path: '/',
 			component: Profile, //우선 개발해야 하는 페이지를 루트로
+		},
+		{
+			path: '/login',
+			component: Login,
 		},
 	],
 });
