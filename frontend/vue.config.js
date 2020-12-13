0207c4f8669cb10dea396bde6efac03dbcfc39b0
YaejinJeon <pikpokjeon@gmail.com>
2020-12-13 22:26:22 +0900

    깃헙 서버 접속 프록시 설정 cors

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,16 @@ module.exports = {
     devServer: {
       overlay: false,
     },
+    
+    devServer: {
+      // 프록시 설정
+      proxy: {
+          // 프록시 요청을 보낼 api의 시작 부분
+          '/oauth/access_token': {
+              // 프록시 요청을 보낼 서버의 주소
+              target: 'https://github.com'
+          }
+      }
+  }
   };
   
