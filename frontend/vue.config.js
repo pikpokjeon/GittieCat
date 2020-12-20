932f3183a96893c483f1b859f8ebda4a0d4fb4e7
ildang100 <last2599@gmail.com>
2020-12-21 00:37:41 +0900

    vue.config 수정
    proxy 제거

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,11 +2,11 @@ module.exports = {
 	devServer: {
 		overlay: false,
 		// 프록시 설정
-		proxy: {
-			// 프록시 요청을 보낼 api의 시작 부분
-			// '/messages/': {
-			// 	target: 'http://localhost:8080/'
-			// }
-		},
+		// proxy: {
+		// 	//프록시 요청을 보낼 api의 시작 부분
+		// 	'/messages/': {
+		// 		target: 'http://localhost:8080/'
+		// 	}
+		// },
 	},
 };
