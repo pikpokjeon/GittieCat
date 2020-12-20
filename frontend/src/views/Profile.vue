8cdb2f7c14fe7629d4a813e48529fcb6154465af
YaejinJeon <pikpokjeon@gmail.com>
2020-12-20 18:01:45 +0900

    remove login test component

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -3,20 +3,17 @@
 		<GameSection></GameSection>
 		<AnalysisSection></AnalysisSection>
 		<RelationshipSection></RelationshipSection>
-		<LoginTest></LoginTest>
 	</section>
 </template>
 <script>
 import GameSection from '@/components/profile/game/GameSection';
 import RelationshipSection from '@/components/profile/relation/RelationshipSection';
 import AnalysisSection from '@/components/profile/analysis/AnalysisSection.vue';
-import LoginTest from '@/components/loginTest/LoginTest.vue';
 export default {
 	components: {
 		GameSection,
 		AnalysisSection,
 		RelationshipSection,
-		LoginTest,
 	},
 };
 </script>
